a698f90f72ac21e80f73ba7e70345bb8550bb6b5
wszdwp <wszdwp@gmail.com>
2013-09-28 17:08:47 -0900

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.  On branch master  Your branch is ahead of 'origin/master' by 1 commit.
     Changes to be committed:
       (use "git reset HEAD <file>..." to unstage)
    
    	new file:   child.c
    	new file:   test
    	new file:   time_client.c
    
     Changes not staged for commit:
       (use "git add <file>..." to update what will be committed)
       (use "git checkout -- <file>..." to discard changes in working directory)
    
    	modified:   child.c
    
     Untracked files:
       (use "git add <file>..." to include in what will be committed)
    
    	.gitignore

---
+++ child.c
@@ -0,0 +1,24 @@
+/*child call exterm to show the commands*/
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+main(int argc, char **argv) {
+
+  pid_t childpid, pid;
+  int   stat;
+
+  printf("forking child\n");
+
+  if ( (childpid = fork()) == 0)  {
+
+      if ( (execlp("xterm", "xterm", "-e", "./test", "127.0.0.1", (char *) 0)) < 0)  {
+            /*print an appropriate error message here*/
+            exit(1);
+         }
+     }
+
+  pid = wait(&stat);  
+  printf("child terminated\n");
+}
